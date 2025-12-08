425d403248f44ea5930311c249940ee7682e8b09
dennis <dliang8@illinois.edu>
2025-12-08 21:49:27 +0900

    p7 p1

---
+++ problem7/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func readInput() [][]string {
+	dat, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	splitted := strings.Split(string(dat), "\n")
+	splitted = splitted[0 : len(splitted)-1]
+	out := [][]string{}
+	for _, s := range splitted {
+		a := []string{}
+		for _, c := range s {
+			a = append(a, string(c))
+		}
+		out = append(out, a)
+	}
+	return out
+}
+
+func printDiagram(input [][]string) {
+	out := []string{}
+	for _, v := range input {
+		out = append(out, strings.Join(v, ""))
+	}
+
+	fmt.Println(strings.Join(out, "\n"))
+}
+
+func drawBeam(x, y int, input [][]string) {
+	if y == len(input) {
+		return
+	}
+	if input[y][x] == "^" {
+		return
+	}
+	input[y][x] = "|"
+}
+
+var outputPositions = map[string]bool{}
+var visited = map[string]bool{}
+var uniqueCarat = map[string]bool{}
+
+func calculateBeam(x, y int, input [][]string) int {
+	if y >= len(input) || x < 0 || x >= len(input[0]) {
+		return 0
+	}
+
+	posKey := fmt.Sprintf("%d,%d", y, x)
+	if visited[posKey] {
+		return 0
+	}
+	visited[posKey] = true
+
+	// search for "S" or "|"
+	if y == 0 {
+		for i := 0; i < len(input[0]); i++ {
+			if input[y][i] == "S" {
+				drawBeam(i, y+1, input)
+				return calculateBeam(i, y+1, input)
+			}
+		}
+	}
+
+	if input[y][x] == "^" {
+		add := 0
+		leftPos := fmt.Sprintf("%d,%d", y, x-1)
+		rightPos := fmt.Sprintf("%d,%d", y, x+1)
+
+		if _, ok := outputPositions[leftPos]; !ok {
+			add++
+			outputPositions[leftPos] = true
+		}
+		if _, ok := outputPositions[rightPos]; !ok {
+			add++
+			outputPositions[rightPos] = true
+		}
+
+		drawBeam(x-1, y, input)
+		drawBeam(x+1, y, input)
+		uniqueCarat[fmt.Sprintf("%d,%d", y, x)] = true
+		// fmt.Println("split at x: ", x, "y:", y)
+		return add + calculateBeam(x-1, y, input) + calculateBeam(x+1, y, input)
+	}
+	drawBeam(x, y+1, input)
+	return calculateBeam(x, y+1, input)
+}
+
+func main() {
+	input := readInput()
+	calculateBeam(0, 0, input)
+	printDiagram(input)
+	fmt.Println(len(uniqueCarat))
+}
